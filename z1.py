c91d96a81ead195dcfd379d53e49e4aa9efa3049
BesedinOleg <Oleg.metro2012@yandex.ru>
2022-05-31 20:43:09 +0900

    release

--- z1.py
+++ z1.py
@@ -1,10 +1,13 @@
-print('Задание 1')
 log = open("access.log", "r")
 arr = list()
 while True:
     s = log.readline()
+    s = s.split()
     if not s:
         break
     if len(arr) < 10:
-        arr.append(s.split())
+        arr.append(s)
+    else:
+        m = arr.index(min(arr, key=lambda x: int(x[4])))
+        arr[m] = max((arr[m], s), key=lambda x: int(x[4]))
 print(*arr, sep='\n')
