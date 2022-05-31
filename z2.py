d7dc7d4b2da05362082fa33636b4597017c656bd
BesedinOleg <Oleg.metro2012@yandex.ru>
2022-06-01 00:37:25 +0900

    save_image_cloud

--- z2.py
+++ z2.py
@@ -12,15 +12,21 @@ user = fake_useragent.UserAgent().random
 header = {'user-agent': user}
 
 linkDate = "https://ncthmwrwbtst.cr.chiba-u.ac.jp/img/FULL_24h/latest.json?_=1653998239673"
-date = requests.post(linkDate, headers=header).text
-date = json.loads(date)
-print(date['date'])
+datetime = requests.post(linkDate, headers=header).text
+date = json.loads(datetime)['date'].split()[0].split('-')
+time = json.loads(datetime)['date'].split()[1].split(':')
+print(date)
+print(time)
 
 linkIm = "https://anzu.sinc.ad.jp/himawari/img/FULL_24h/BlueMarble/1d/385/BlueMarble_0_0.png"
 img_data = requests.get(linkIm).content
-with open('wallpaper.jpg', 'wb') as handler:
+with open('earth.png', 'wb') as handler:
     handler.write(img_data)
 
+linkIm = "https://anzu.sinc.ad.jp/himawari/img/FULL_24h/B13/1d/550/" + date[0] + "/" + date[1] + "/" + date[2] + "/" + ''.join(time) + "_0_0.png"
+img_data = requests.get(linkIm).content
+with open('cloud.png', 'wb') as handler:
+    handler.write(img_data)
 
 
 
