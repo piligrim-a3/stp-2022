f9aaa544a37b05c888ff34646c18056687aa9ada
BesedinOleg <Oleg.metro2012@yandex.ru>
2022-05-31 18:54:42 +0900

    read file

--- z1.py
+++ z1.py
@@ -1,1 +1,10 @@
-print('111')
+print('Задание 1')
+log = open("access.log", "r")
+arr = list()
+while True:
+    s = log.readline()
+    if not s:
+        break
+    if len(arr) < 10:
+        arr.append(s.split())
+print(*arr, sep='\n')
