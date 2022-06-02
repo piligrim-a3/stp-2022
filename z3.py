c4343ab3deffba1b83b010d919d2bba269f9fbae
BesedinOleg <Oleg.metro2012@yandex.ru>
2022-06-02 18:38:54 +0900

    def

--- z3.py
+++ z3.py
@@ -2,14 +2,40 @@ import json
 import requests
 import fake_useragent
 import qrcode
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
+from PyQt5.QtWidgets import QMainWindow
 
+import sys
+
+#lgn: 1558
+#pwd: n6hgur
 session = requests.Session()
 user = fake_useragent.UserAgent().random
 header = {'user-agent': user}
-link = "https://bgpu.ru/campus/api/v1/auth"
-data = {'login': '1558',
-        'pwd': 'n6hgur'}
-authorization = session.post(link, data=data, headers=header)
-link = "https://lk.bgpu.ru/campus/api/v1/area/qr?index=1"
-indexes = session.get(link, headers=header).text
-data = json.loads(indexes)
+
+
+def login(lgn, pwd):
+        link = "https://bgpu.ru/campus/api/v1/auth"
+        data = {'login': lgn,
+                'pwd': pwd}
+        session.post(link, data=data, headers=header)
+
+
+def new_qr():
+        link = "https://lk.bgpu.ru/campus/api/v1/area/qr?index=1"
+        indexes = session.get(link, headers=header).text
+        return json.loads(indexes)
+
+
+def valid_qr(jqr):
+        print(*jqr, sep='\n')
+        actual_code_link = "https://lk.bgpu.ru/campus/api/v1/area/counter-index"
+        code = session.get(actual_code_link, headers=header).text
+        return json.loads(code)
+
+
+def create_qr(jqr):
+        img = qrcode.make(jqr[-1]['qr'])
+        print(jqr[-1]['qr'])
+        type(img)
+        img.save("qr.png")
