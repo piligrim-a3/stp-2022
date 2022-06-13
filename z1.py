d495b202adfce6c477aa609ca7ad0f48f97d1e03
BesedinOleg <Oleg.metro2012@yandex.ru>
2022-06-13 19:51:55 +0900

    z1_out.txt

--- z1.py
+++ z1.py
@@ -14,4 +14,9 @@ while True:
         '''Сравниваем с минимальным элементом текущий, из читаемой строки'''
         m = arr.index(min(arr, key=lambda x: int(x[4])))
         arr[m] = max((arr[m], s), key=lambda x: int(x[4]))
-print(*arr, sep='\n')
+'''сортировка по убыванию'''
+arr = sorted(arr, key=lambda x: -int(x[4]))
+'''Запись в файл'''
+f = open('out.txt', 'w')
+for s in arr:
+    f.write(" ".join(s) + '\n')
