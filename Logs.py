969f9605216186843a01e764ce1d8620e5fabce8
Keyd1204 <106795345+Keyd1204@users.noreply.github.com>
2022-06-14 19:52:15 +0900

    Обработали файл и поместили результат в массив
    1 шаг - замена в переменной data одного текста на другой
    2 шаг - разделение строковой переменной на массив. Разделителем элементов массива является пробел

--- Logs.py
+++ Logs.py
@@ -9,6 +9,16 @@ with zipfile.ZipFile(archive, 'r') as zip_file:
 
 #открытие распакованого файла с логами access.log
 f = open("extract/access.log")
-
 #назначение текстовой переменной текстового содержимого с файла access.log
 data = f.read()
+
+#замена в переменной data одного текста на другой
+#data.replace('-', '')
+data = data.replace('-', '')
+data = data.replace('    ', ' ')
+data = data.replace('  ', ' ')
+data = data.replace('  ', ' ')
+data = data.replace('  ', ' ')
+data = data.replace('\n', ' ')
+#разделяем строковую переменную на массив. Разделителем является пробел
+data = data.split(" ")
