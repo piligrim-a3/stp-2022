030da80c5337c3e07d0e66c542fbd018365b9653
Keyd1204 <106795345+Keyd1204@users.noreply.github.com>
2022-06-14 20:26:29 +0900

    Заполнение базы данных
    заполнение таблицы базы данных исходя из значений массива. Каждая новая строка состоит из 9 элементов массива

--- Logs.py
+++ Logs.py
@@ -38,3 +38,16 @@ cur.execute("""CREATE TABLE IF NOT EXISTS spisok(
       type TEXT);
    """)
 conn.commit()
+
+#переменная счетчик
+i = 0
+
+#заполняем базу данных. каждая строка в файле логов - 1 строка в базе данных
+while(i<len(data)):
+    cur.execute(
+        "INSERT INTO spisok (time, elapsed, remotehost, code_status, bytes, method, URL, peerstatus_peerhost, type) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        (data[i], data[i+1], data[i+2], data[i+3], data[i+4], data[i+5],
+         data[i+6], data[i+7], data[i+8]))
+    conn.commit()
+    #плюсуем к счетчику единицу
+    i = i + 9
