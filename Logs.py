89ea2930ac873f2599d782d722d455e6519d8909
Keyd1204 <106795345+Keyd1204@users.noreply.github.com>
2022-06-14 20:10:11 +0900

    База данных. Создание
    Создание базы с указанием типов данных и названий колонок. Сама база данных нужна для корректной сортировки данных по целевому трафику и в последующей удобной выгрузкой 10 значений с наибольшими цифрами

--- Logs.py
+++ Logs.py
@@ -22,3 +22,19 @@ data = data.replace('  ', ' ')
 data = data.replace('\n', ' ')
 #разделяем строковую переменную на массив. Разделителем является пробел
 data = data.split(" ")
+
+#создаем базу logi.db и конектимся к ней, а так же таблицу с названием spisok
+conn = sqlite3.connect('logi.db')
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS spisok(
+      time TEXT,
+      elapsed TEXT,
+      remotehost TEXT,
+      code_status TEXT,
+      bytes INT,
+      method TEXT,
+      URL TEXT,
+      peerstatus_peerhost TEXT,
+      type TEXT);
+   """)
+conn.commit()
