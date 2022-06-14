34d67829d531be9d8f840aaa35b680b4c7a2d444
Keyd1204 <106795345+Keyd1204@users.noreply.github.com>
2022-06-14 22:12:53 +0900

    Начало работы кода (работа с изображением)
    Определяем текущую дату, указав путь, парсим изображение с сайта по параметрам и сохраняем изображение (дл удобства выведем в консоль полезную информацию)

--- zad2/zad2.py
+++ zad2/zad2.py
@@ -3,3 +3,18 @@ import struct
 import ctypes
 import os
 import datetime
+
+#определение текущей даты
+date = datetime.datetime.today()
+
+#парсим изображение
+imgURL = "https://ncthmwrwbtst.cr.chiba-u.ac.jp/img/D531106/thumbnail/550/"+date.strftime('%Y')+"/"+date.strftime('%m')+"/"+date.strftime('%d')+"/090000_0_0.png"
+print(imgURL)
+
+#путь целевого изображения где мы его сохраняем
+put = "C:/Users/"+os.environ.get( "USERNAME" )+"/Desktop/123.jpg"
+urllib.request.urlretrieve(imgURL, put)
+
+print(put)
+PATH = put
+#PATH = 'C:\\image.jpg' 
