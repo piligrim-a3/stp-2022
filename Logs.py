0065a56323b10eac90e14b540bb6e977be60479a
Keyd1204 <106795345+Keyd1204@users.noreply.github.com>
2022-06-14 19:35:44 +0900

    Работа с архивом
    Извлечение лог-файла из архива и поместим содержимое в строковую переменную

--- Logs.py
+++ Logs.py
@@ -2,3 +2,13 @@ import zipfile
 import sqlite3
 import os
 
+#разархивирование архива access.zip
+archive = 'access.zip'
+with zipfile.ZipFile(archive, 'r') as zip_file:
+    zip_file.extractall("extract")
+
+#открытие распакованого файла с логами access.log
+f = open("extract/access.log")
+
+#назначение текстовой переменной текстового содержимого с файла access.log
+data = f.read()
