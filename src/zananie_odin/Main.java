87f036fca058199e0832e2f6b1d4ec34215fac6b
Vladosyi <krivega007@gmail.com>
2022-09-23 18:38:42 +0900

    add ip and bytes in map

--- src/zananie_odin/Main.java
+++ src/zananie_odin/Main.java
@@ -1,7 +1,32 @@
 package zananie_odin;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        Map<String, Integer> logs = new HashMap<>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("access.log"));
+            String line;
+            String ip;
+            int bytes;
+            while ((line = bufferedReader.readLine()) != null) {
+                //если встретится какое либо количество пробелов, то они будут разделителями
+                ip = line.split(" +")[2]; //Третий элемент ip-адрес
+                bytes = Integer.parseInt(line.split(" +")[4]); //пятый элемент это колво байт
+                logs.put(ip, bytes);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        System.out.println(logs);
     }
 }
